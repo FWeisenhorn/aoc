c61cfc99c1051bc89c119242939f96805fa83404
FWeisenhorn <f.weisenhorn@pm.me>
2023-12-14 19:42:18 +0100

    Day 14 finished, with part 1 changed

--- src/aoc2023/day14.rs
+++ src/aoc2023/day14.rs
@@ -1,4 +1,4 @@
-type Pos = (usize, usize);
+use std::collections::HashMap;
 
 const INPUT: &str = include_str!("inputs/day14.txt");
 
@@ -8,52 +8,117 @@ pub fn run() {
 }
 
 fn part_a(input: &str) -> String {
-    let stable_rocks = get_rocks_from_input(input, '#');
-    let mut movable_rocks = get_rocks_from_input(input, 'O');
+    let mut data: Vec<char> = input.lines().flat_map(str::chars).collect();
+    let height = input.lines().count();
 
-    movable_rocks.sort_unstable_by_key(|&(x, _)| x);
+    tilt_north(&mut data, height);
 
-    let mut has_changed = true;
-    while has_changed {
-        has_changed = false;
-        let mut temp_copy = movable_rocks.clone();
-        for rock in &mut temp_copy {
-            if rock.0 == 0
-                || stable_rocks.contains(&(rock.0 - 1, rock.1))
-                || movable_rocks.contains(&(rock.0 - 1, rock.1))
-            {
-                ()
-            } else {
-                has_changed = true;
-                rock.0 -= 1;
-            }
-        }
-        movable_rocks = temp_copy;
-    }
-
-    let max_x = stable_rocks.iter().max_by_key(|(x, _)| x).unwrap().0;
-
-    movable_rocks
-        .iter()
-        .map(|&(x, _)| max_x - x + 1)
-        .sum::<usize>()
-        .to_string()
+    get_answer(&data, height)
 }
 
 fn part_b(input: &str) -> String {
-    String::new()
+    let mut data: Vec<char> = input.lines().flat_map(str::chars).collect();
+    let height = input.lines().count();
+
+    let spin_cycles = 1_000_000_000u32;
+
+    let mut seen: HashMap<Vec<char>, u32> = HashMap::new();
+
+    for i in 0..spin_cycles {
+        if let Some(seen_at) = seen.insert(data.clone(), i) {
+            if (spin_cycles - i) % (i - seen_at) == 0 {
+                break;
+            }
+        }
+
+        tilt_north(&mut data, height);
+        tilt_west(&mut data, height);
+        tilt_south(&mut data, height);
+        tilt_east(&mut data, height);
+    }
+
+    get_answer(&data, height)
 }
 
-fn get_rocks_from_input(input: &str, input_c: char) -> Vec<Pos> {
-    let mut rocks = Vec::new();
-    input.lines().enumerate().for_each(|(i, line)| {
-        line.chars().enumerate().for_each(|(j, c)| {
-            if c == input_c {
-                rocks.push((i, j));
-            };
-        });
-    });
-    rocks
+fn get_answer(data: &Vec<char>, height: usize) -> String {
+    let width = data.len() / height;
+    data.iter()
+        .enumerate()
+        .filter(|(_, &c)| c == 'O')
+        .fold(0usize, |acc, (i, _)| acc + (height - i / width))
+        .to_string()
+}
+
+fn tilt_north(data: &mut Vec<char>, height: usize) {
+    let width = data.len() / height;
+    let mut has_changed = true;
+    while has_changed {
+        has_changed = false;
+        for x in 0..height - 1 {
+            for y in 0..width {
+                let coord = x * width + y;
+                if data[coord] == '.' && data[coord + width] == 'O' {
+                    data[coord] = 'O';
+                    data[coord + width] = '.';
+                    has_changed = true;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_west(data: &mut Vec<char>, height: usize) {
+    let width = data.len() / height;
+    let mut has_changed = true;
+    while has_changed {
+        has_changed = false;
+        for x in 0..height {
+            for y in 0..width - 1 {
+                let coord = x * width + y;
+                if data[coord] == '.' && data[coord + 1] == 'O' {
+                    data[coord] = 'O';
+                    data[coord + 1] = '.';
+                    has_changed = true;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_south(data: &mut Vec<char>, height: usize) {
+    let width = data.len() / height;
+    let mut has_changed = true;
+    while has_changed {
+        has_changed = false;
+        for x in (1..height).rev() {
+            for y in (0..width).rev() {
+                let coord = x * width + y;
+                if data[coord] == '.' && data[coord - width] == 'O' {
+                    data[coord] = 'O';
+                    data[coord - width] = '.';
+                    has_changed = true;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_east(data: &mut Vec<char>, height: usize) {
+    let width = data.len() / height;
+    let mut has_changed = true;
+    while has_changed {
+        has_changed = false;
+        for x in (0..height).rev() {
+            for y in (1..width).rev() {
+                let coord = x * width + y;
+                if data[coord] == '.' && data[coord - 1] == 'O' {
+                    data[coord] = 'O';
+                    data[coord - 1] = '.';
+                    has_changed = true;
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -62,7 +127,7 @@ mod tests {
 
     const _TEST: &str = include_str!("tests/day14.txt");
     const _TESTRESULT_A: &str = "136";
-    const _TESTRESULT_B: &str = "";
+    const _TESTRESULT_B: &str = "64";
 
     #[test]
     fn test_a() {
