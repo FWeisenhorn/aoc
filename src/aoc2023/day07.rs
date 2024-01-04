10fe05ecf3d3adc192c00ed87072d69627b1e49d
FWeisenhorn <f.weisenhorn@pm.me>
2024-01-04 22:36:48 +0100

    Rewrote Day 7

--- src/aoc2023/day07.rs
+++ src/aoc2023/day07.rs
@@ -1,6 +1,7 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::marker::PhantomData;
+use std::ops::AddAssign;
 
 const INPUT: &str = include_str!("inputs/day07.txt");
 
@@ -10,232 +11,160 @@ pub fn run() {
 }
 
 fn part_a(input: &str) -> String {
-    let mut input: Vec<(Hand<StandardDeck>, usize)> = input
-        .lines()
-        .map(|line| {
-            let x = line.split_at(5);
-            (
-                Hand::<StandardDeck>::try_from(x.0).unwrap(),
-                x.1[1..].parse().unwrap(),
-            )
-        })
-        .collect();
+    let mut hands: Vec<(Hand<StandardDeck>, usize)> = read_input(input);
 
-    input.sort_unstable_by_key(|(h, _)| *h);
+    hands.sort_unstable_by_key(|(h, _)| h.clone());
 
-    input
-        .iter()
-        .enumerate()
-        .fold(0, |acc, (i, (_, x))| acc + (i + 1) * *x)
-        .to_string()
+    count_result(&hands)
 }
 
 fn part_b(input: &str) -> String {
-    let mut input: Vec<(Hand<JokerDeck>, usize)> = input
+    let mut hands: Vec<(Hand<JokerDeck>, usize)> = read_input(input);
+
+    hands.sort_unstable_by_key(|(h, _)| h.clone());
+
+    count_result(&hands)
+}
+
+fn read_input<T: DeckType>(input: &str) -> Vec<(Hand<T>, usize)> {
+    input
         .lines()
         .map(|line| {
             let x = line.split_at(5);
-            (
-                Hand::<JokerDeck>::try_from(x.0).unwrap(),
-                x.1[1..].parse().unwrap(),
-            )
+            (Hand::new(x.0), x.1[1..].parse().unwrap())
         })
-        .collect();
+        .collect()
+}
 
-    input.sort_unstable_by_key(|(h, _)| *h);
-
-    input
+fn count_result<T: DeckType>(hands: &[(Hand<T>, usize)]) -> String {
+    hands
         .iter()
         .enumerate()
-        .fold(0, |acc, (i, (_, x))| acc + (i + 1) * *x)
+        .fold(0, |acc, (i, &(_, x))| acc + (i + 1) * x)
         .to_string()
 }
 
-#[derive(PartialEq, Eq, Clone, Copy, Hash)]
-struct StandardDeck;
-#[derive(PartialEq, Eq, Clone, Copy, Hash)]
-struct JokerDeck;
-trait DeckType {
-    fn deck() -> [char; 13]
-    where
-        Self: Sized;
+trait DeckType: Eq + Ord {
+    fn card_value(x: char) -> Option<u8>;
+
+    fn compare_cards(a: char, b: char) -> Ordering {
+        Self::card_value(a).cmp(&Self::card_value(b))
+    }
+
+    fn hand_ordering(occ: &HashMap<&u8, u8>) -> [u8; 2];
 }
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone)]
+struct StandardDeck;
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone)]
+struct JokerDeck;
+
 impl DeckType for StandardDeck {
-    fn deck() -> [char; 13] {
+    fn card_value(x: char) -> Option<u8> {
         [
             '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
         ]
+        .iter()
+        .position(|&t| t == x)
+        .map(|x| u8::try_from(x).unwrap())
+    }
+
+    fn hand_ordering(occ: &HashMap<&u8, u8>) -> [u8; 2] {
+        let mut t: Vec<_> = occ.values().collect();
+
+        t.sort_unstable();
+
+        match (t.pop(), t.pop()) {
+            (Some(5), None) => [5, 0],
+            (Some(&x), Some(&y)) => [x, y],
+            _ => unreachable!(),
+        }
     }
 }
 
 impl DeckType for JokerDeck {
-    fn deck() -> [char; 13] {
+    fn card_value(x: char) -> Option<u8> {
         [
             'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
         ]
+        .iter()
+        .position(|&t| t == x)
+        .map(|x| u8::try_from(x).unwrap())
     }
-}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct Card<DeckType: ?Sized> {
-    card_type: char,
-    _d: PhantomData<DeckType>,
-}
+    fn hand_ordering(occ: &HashMap<&u8, u8>) -> [u8; 2] {
+        let mut occ = occ.clone();
 
-impl<T: DeckType + std::cmp::Eq> PartialOrd for Card<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<T: DeckType + std::cmp::Eq> Ord for Card<T> {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        T::deck()
-            .iter()
-            .position(|c| c == &self.card_type)
-            .cmp(&T::deck().iter().position(|c| c == &other.card_type))
-    }
-}
-
-impl<T: DeckType> TryFrom<char> for Card<T> {
-    type Error = &'static str;
-
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        T::deck()
-            .contains(&value)
-            .then_some(Self {
-                card_type: value,
-                _d: PhantomData,
-            })
-            .ok_or("Card creation failed, maybe wrong char?")
-    }
-}
-
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
-struct Hand<DeckType> {
-    cards: [Card<DeckType>; 5],
-}
-
-impl Ord for Hand<StandardDeck> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let t1 = self.occurrences();
-        let t2 = other.occurrences();
-
-        if t1.0 == t2.0 {
-            if t1.1 == t2.1 {
-                self.cards
-                    .iter()
-                    .zip(other.cards.iter())
-                    .map(|(a, b)| a.cmp(b))
-                    .find(|x| *x != Ordering::Equal)
-                    .expect("Equality between the two hands!")
-            } else {
-                t1.1.cmp(&t2.1)
-            }
-        } else {
-            t1.0.cmp(&t2.0)
-        }
-    }
-}
-
-impl PartialOrd for Hand<StandardDeck> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Hand<JokerDeck> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let t1 = self.occurrences();
-        let t2 = other.occurrences();
-
-        if t1.0 == t2.0 {
-            if t1.1 == t2.1 {
-                self.cards
-                    .iter()
-                    .zip(other.cards.iter())
-                    .map(|(a, b)| a.cmp(b))
-                    .find(|x| *x != Ordering::Equal)
-                    .expect("Equality between the two hands!")
-            } else {
-                t1.1.cmp(&t2.1)
-            }
-        } else {
-            t1.0.cmp(&t2.0)
-        }
-    }
-}
-
-impl PartialOrd for Hand<JokerDeck> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<T: DeckType> TryFrom<&str> for Hand<T> {
-    type Error = &'static str;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        // assert_eq!(value.len(), 5);
-        value
-            .chars()
-            .flat_map(Card::try_from)
-            .collect::<Vec<_>>()
-            .try_into()
-            .map_or(Err("Conversion failed, maybe not five cards?"), |x| {
-                Ok(Self { cards: x })
-            })
-    }
-}
-
-impl Hand<StandardDeck> {
-    fn occurrences(&self) -> (usize, usize) {
-        let t: HashMap<_, _> = self.cards.iter().fold(HashMap::new(), |mut acc, c| {
-            acc.entry(c).and_modify(|x| *x += 1).or_insert(1);
-            acc
-        });
-
-        let mut t: Vec<usize> = t.into_values().collect();
-
-        t.sort_unstable();
-
-        match (t.pop(), t.pop()) {
-            (Some(5), None) => (5, 0),
-            (Some(x), Some(y)) => (x, y),
-            _ => unreachable!(),
-        }
-    }
-}
-
-impl Hand<JokerDeck> {
-    fn occurrences(&self) -> (usize, usize) {
-        let mut t: HashMap<_, _> = self.cards.iter().fold(HashMap::new(), |mut acc, c| {
-            acc.entry(c).and_modify(|x| *x += 1).or_insert(1);
-            acc
-        });
-
-        let j = t
-            .remove(&Card {
-                card_type: 'J',
-                _d: PhantomData,
-            })
-            .unwrap_or(0);
-
-        let mut t: Vec<usize> = t.into_values().collect();
-
+        let j = occ.remove(&Self::card_value('J').unwrap()).unwrap_or(0);
         if j == 5 {
-            return (5, 0);
+            return [5, 0];
         }
+
+        let mut t: Vec<_> = occ.into_values().collect();
 
         t.sort_unstable();
 
         match (t.pop(), t.pop()) {
-            (Some(_), None) => (5, 0),
-            (Some(x), Some(y)) => (x + j, y),
+            (Some(_), None) => [5, 0],
+            (Some(x), Some(y)) => [x + j, y],
             _ => unreachable!(),
         }
     }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+struct Hand<T: DeckType> {
+    // cards: [u8; 5],
+    values: [u8; 7],
+    deck_type: PhantomData<T>,
+}
+
+impl<T: DeckType> Ord for Hand<T> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.values
+            .iter()
+            .zip(other.values)
+            .map(|(a, b)| a.cmp(&b))
+            .find(|&x| x != Ordering::Equal)
+            .unwrap()
+    }
+}
+
+impl<T: DeckType> PartialOrd for Hand<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<T: DeckType> Hand<T> {
+    fn new(s: &str) -> Self {
+        assert_eq!(s.len(), 5);
+
+        let t = s
+            .chars()
+            .map(|c| T::card_value(c).unwrap())
+            .collect::<Vec<u8>>();
+
+        Self {
+            values: occurrences::<T>(&t)
+                .into_iter()
+                .chain(t)
+                .collect::<Vec<u8>>()
+                .try_into()
+                .unwrap(),
+            deck_type: PhantomData,
+        }
+    }
+}
+
+fn occurrences<T: DeckType>(cards: &[u8]) -> [u8; 2] {
+    let t: HashMap<_, u8> = cards.iter().fold(HashMap::new(), |mut acc, c| {
+        acc.entry(c).or_insert(0).add_assign(1);
+        acc
+    });
+
+    T::hand_ordering(&t)
 }
 
 #[cfg(test)]
